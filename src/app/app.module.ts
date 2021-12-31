302b01a85d369064d82ad6f6f354f66f3652e14f
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2021-12-31 15:11:05 +0600

    Created basic app structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageComponent } from './messages/message/message.component';
+import { NewMessageComponent } from './messages/new-message/new-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessagesComponent,
+    MessageComponent,
+    NewMessageComponent
   ],
   imports: [
     BrowserModule
